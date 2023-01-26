ae90872af1e89e48de5d3d6ca424e51fbf6bd39b
Marlon Monroy <saito.sv.h@gmail.com>
2023-01-25 18:49:29 -0800

    added redis store

--- database/redis.go
+++ database/redis.go
@@ -2,9 +2,12 @@ package database
 
 import (
 	"context"
+	"crypto/tls"
+	"log"
 	"time"
 
-	"github.com/go-redis/redis"
+	"github.com/String-xyz/go-lib/common"
+	"github.com/go-redis/redis/v8"
 )
 
 type RedisRepresentable interface {
@@ -26,4 +29,134 @@ type RedisStore interface {
 	HDel(string, string) int64
 	HMLen(string) int64
 	Delete(string) error
+}
+
+type redisStore struct {
+	client RedisRepresentable
+}
+
+type RedisConfigOptions struct {
+	ClusterMode bool
+	Port        string
+	Host        string
+	Password    string
+}
+
+const REDIS_NOT_FOUND_ERROR = "redis: nil"
+
+func redisTLSConf() *tls.Config {
+	var tlsCf *tls.Config
+	if !common.IsLocalEnv() {
+		tlsCf = &tls.Config{
+			MinVersion: tls.VersionTLS12,
+		}
+	}
+
+	return tlsCf
+}
+
+func redisOptions(host, port, password string) *redis.Options {
+	url := host + ":" + port
+	var tlsCf *tls.Config
+	if !common.IsLocalEnv() {
+		tlsCf = &tls.Config{
+			MinVersion: tls.VersionTLS12,
+		}
+	}
+
+	op := &redis.Options{
+		Addr:      url,
+		TLSConfig: tlsCf,
+		Password:  password,
+		DB:        0,
+	}
+	return op
+}
+
+func cluster(host, port, password string) *redis.ClusterClient {
+	url := host + ":" + port
+	return redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs:          []string{url},
+		Password:       password,
+		PoolSize:       10,
+		MinIdleConns:   10,
+		TLSConfig:      redisTLSConf(),
+		ReadOnly:       false,
+		RouteRandomly:  false,
+		RouteByLatency: false,
+	})
+}
+
+func NewRedisStore(options RedisConfigOptions) RedisStore {
+	ctx := context.Background()
+	var client RedisRepresentable
+	if !options.ClusterMode {
+		client = redis.NewClient(redisOptions(options.Host, options.Port, options.Password))
+	} else {
+		client = cluster(options.Host, options.Port, options.Password)
+	}
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Failed to ping Redis: %v", err)
+	}
+
+	return &redisStore{
+		client: client,
+	}
+}
+
+func (r redisStore) Delete(id string) error {
+	ctx := context.Background()
+	_, err := r.client.Del(ctx, id).Result()
+	if err != nil {
+		return common.StringError(err)
+	}
+	return nil
+}
+
+func (r redisStore) Get(id string) ([]byte, error) {
+	ctx := context.Background()
+	bytes, err := r.client.Get(ctx, id).Bytes()
+	if err != nil {
+		return nil, common.StringError(err)
+	}
+	return bytes, nil
+}
+
+func (r redisStore) Set(id string, value any, expire time.Duration) error {
+	ctx := context.Background()
+	if err := r.client.Set(ctx, id, value, expire).Err(); err != nil {
+		return common.StringError(err)
+	}
+	return nil
+}
+
+func (r redisStore) HSet(key string, data map[string]interface{}) error {
+	ctx := context.Background()
+	if err := r.client.HSet(ctx, key, data).Err(); err != nil {
+		return common.StringError(err, "failed to save array to redis")
+	}
+
+	return nil
+}
+
+func (r redisStore) HGetAll(key string) (map[string]string, error) {
+	ctx := context.Background()
+	data, err := r.client.HGetAll(ctx, key).Result()
+	if err != nil {
+		return data, common.StringError(err)
+	}
+	return data, nil
+}
+
+func (r redisStore) HMLen(key string) int64 {
+	ctx := context.Background()
+	data := r.client.HLen(ctx, key)
+	return data.Val()
+}
+
+func (r redisStore) HDel(key, val string) int64 {
+	ctx := context.Background()
+	data := r.client.HDel(ctx, key, val)
+	return data.Val()
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,10 @@ go 1.19
 
 require (
 	github.com/go-playground/validator/v10 v10.11.1
-	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/redis/v8 v8.0.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/labstack/echo/v4 v4.10.0
+	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.28.0
 	github.com/stretchr/testify v1.8.1
 	gopkg.in/DataDog/dd-trace-go.v1 v1.46.1
@@ -23,6 +24,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgraph-io/ristretto v0.1.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
@@ -39,15 +41,16 @@ require (
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.25.0 // indirect
 	github.com/philhofer/fwd v1.1.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/secure-systems-lab/go-securesystemslib v0.4.0 // indirect
 	github.com/tinylib/msgp v1.1.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.opentelemetry.io/otel v0.11.0 // indirect
 	go4.org/intern v0.0.0-20211027215823-ae77deb06f29 // indirect
 	go4.org/unsafe/assume-no-moving-gc v0.0.0-20220617031537-928513b29760 // indirect
 	golang.org/x/crypto v0.2.0 // indirect
+	golang.org/x/exp v0.0.0-20200908183739-ae8ad444f925 // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
