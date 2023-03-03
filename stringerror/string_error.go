ccab1c5c4d0cb3d173205f4eaeabc77aeb9f8fdb
Wilfredo Alcala <alcalawilfre@gmail.com>
2023-03-03 17:05:11 -0500

    comment out marlon's proposal

--- stringerror/string_error.go
+++ stringerror/string_error.go
@@ -73,71 +73,74 @@ func ErrorIs(err, target error) bool {
 	return errors.Cause(err).Error() == target.Error()
 }
 
-// SError -> String Error
-type SError struct {
-	Code        string
-	Message     string
-	NativeError error
-}
+var NOT_FOUND = errors.New("not found")
+var FORBIDDEN = errors.New("invoking member lacks authority")
+var INVALID_RESET_TOKEN = errors.New("invalid password reset token")
+var INVALID_PASSWORD = errors.New("invalid password")
+var ALREADY_IN_USE = errors.New("already in use")
+var DEACTIVATED = errors.New("deactivated")
+var REDIS_NOT_FOUND = errors.New("redis not found")
 
-func (e SError) Error() string {
-	return e.Code
-}
+/* Marlon's Proposal */
 
-func (e SError) UnWrap() error {
-	return e.NativeError
-}
+// // SError -> String Error
+// type SError struct {
+// 	Code        string
+// 	Message     string
+// 	NativeError error
+// 	StackTrace  errors.StackTrace
+// }
 
-func New(code, message string) SError {
-	return SError{Code: code, Message: message, NativeError: errors.New(code)}
-}
+// func (e SError) Error() string {
+// 	return e.Code
+// }
 
-var NOT_FOUND = SError{
-	Code:        "not_found",
-	Message:     "not found",
-	NativeError: errors.New("not found"),
-}
+// func (e SError) UnWrap() error {
+// 	return e.NativeError
+// }
 
-var FORBIDDEN = SError{
-	Code:        "forbidden",
-	Message:     "invoking member lacks authority",
-	NativeError: errors.New("invoking member lacks authority"),
-}
+// func New(code, message string) SError {
+// 	return SError{Code: code, Message: message, NativeError: errors.New(code)}
+// }
 
-var INVALID_RESET_TOKEN = SError{
-	Code:        "invalid_reset_token",
-	Message:     "invalid password reset token",
-	NativeError: errors.New("invalid password reset token"),
-}
+// var NOT_FOUND = SError{
+// 	Code:        "not_found",
+// 	Message:     "not found",
+// 	NativeError: errors.New("not found"),
+// }
 
-var INVALID_PASSWORD = SError{
-	Code:        "invalid_password",
-	Message:     "invalid password",
-	NativeError: errors.New("invalid password"),
-}
+// var FORBIDDEN = SError{
+// 	Code:        "forbidden",
+// 	Message:     "invoking member lacks authority",
+// 	NativeError: errors.New("invoking member lacks authority"),
+// }
 
-var ALREADY_IN_USE = SError{
-	Code:        "already_in_use",
-	Message:     "already in use",
-	NativeError: errors.New("already in use"),
-}
+// var INVALID_RESET_TOKEN = SError{
+// 	Code:        "invalid_reset_token",
+// 	Message:     "invalid password reset token",
+// 	NativeError: errors.New("invalid password reset token"),
+// }
 
-var DEACTIVATED = SError{
-	Code:        "deactivated",
-	Message:     "deactivated",
-	NativeError: errors.New("deactivated"),
-}
+// var INVALID_PASSWORD = SError{
+// 	Code:        "invalid_password",
+// 	Message:     "invalid password",
+// 	NativeError: errors.New("invalid password"),
+// }
 
-var REDIS_NOT_FOUND = SError{
-	Code:        "redis_not_found",
-	Message:     "redis not found",
-	NativeError: errors.New("redis not found"),
-}
+// var ALREADY_IN_USE = SError{
+// 	Code:        "already_in_use",
+// 	Message:     "already in use",
+// 	NativeError: errors.New("already in use"),
+// }
 
-// var NOT_FOUND = errors.New("not found")
-// var FORBIDDEN = errors.New("invoking member lacks authority")
-// var INVALID_RESET_TOKEN = errors.New("invalid password reset token")
-// var INVALID_PASSWORD = errors.New("invalid password")
-// var ALREADY_IN_USE = errors.New("already in use")
-// var DEACTIVATED = errors.New("deactivated")
-// var REDIS_NOT_FOUND = errors.New("redis not found")
+// var DEACTIVATED = SError{
+// 	Code:        "deactivated",
+// 	Message:     "deactivated",
+// 	NativeError: errors.New("deactivated"),
+// }
+
+// var REDIS_NOT_FOUND = SError{
+// 	Code:        "redis_not_found",
+// 	Message:     "redis not found",
+// 	NativeError: errors.New("redis not found"),
+// }
