31e8cbdd093f08327dda96de85cb939fe8bd20c7
Wilfredo Alcala <alcalawilfre@gmail.com>
2023-04-25 21:44:31 -0300

    Soft Delete (#19)

--- repository/repository.go
+++ repository/repository.go
@@ -61,15 +61,15 @@ func (b Base[T]) List(ctx context.Context, limit int, offset int) (list []T, err
 		limit = 20
 	}
 
-	err = b.Store.SelectContext(ctx, &list, fmt.Sprintf("SELECT * FROM %s LIMIT $1 OFFSET $2", b.Table), limit, offset)
+	err = b.Store.SelectContext(ctx, &list, fmt.Sprintf("SELECT * FROM %s WHERE deleted_at IS NULL LIMIT $1 OFFSET $2", b.Table), limit, offset)
 	if err == sql.ErrNoRows {
 		return list, nil
 	}
 	return list, err
 }
 
-func (b Base[T]) GetById(ctx context.Context, ID string) (m T, err error) {
-	err = b.Store.GetContext(ctx, &m, fmt.Sprintf("SELECT * FROM %s WHERE id = $1 AND deactivated_at IS NULL", b.Table), ID)
+func (b Base[T]) GetById(ctx context.Context, id string) (m T, err error) {
+	err = b.Store.GetContext(ctx, &m, fmt.Sprintf("SELECT * FROM %s WHERE id = $1 AND deleted_at IS NULL", b.Table), id)
 	if err != nil && err == sql.ErrNoRows {
 		return m, serror.NOT_FOUND
 	}
@@ -77,20 +77,19 @@ func (b Base[T]) GetById(ctx context.Context, ID string) (m T, err error) {
 }
 
 // Returns the first match of the user's ID
-func (b Base[T]) GetByUserId(ctx context.Context, userID string) (m T, err error) {
-	err = b.Store.GetContext(ctx, &m, fmt.Sprintf("SELECT * FROM %s WHERE user_id = $1 AND deactivated_at IS NULL LIMIT 1", b.Table), userID)
+func (b Base[T]) GetByUserId(ctx context.Context, userId string) (m T, err error) {
+	err = b.Store.GetContext(ctx, &m, fmt.Sprintf("SELECT * FROM %s WHERE user_id = $1 AND deleted_at IS NULL LIMIT 1", b.Table), userId)
 	if err != nil && err == sql.ErrNoRows {
 		return m, serror.NOT_FOUND
 	}
 	return m, err
 }
-
-func (b Base[T]) ListByUserId(ctx context.Context, userID string, limit int, offset int) ([]T, error) {
+func (b Base[T]) ListByUserId(ctx context.Context, userId string, limit int, offset int) ([]T, error) {
 	list := []T{}
 	if limit == 0 {
 		limit = 100
 	}
-	err := b.Store.SelectContext(ctx, &list, fmt.Sprintf("SELECT * FROM %s WHERE user_id = $1 LIMIT $2 OFFSET $3", b.Table), userID, limit, offset)
+	err := b.Store.SelectContext(ctx, &list, fmt.Sprintf("SELECT * FROM %s WHERE user_id = $1 AND deleted_at IS NULL LIMIT $2 OFFSET $3", b.Table), userId, limit, offset)
 	if err == sql.ErrNoRows {
 		return list, nil
 	}
@@ -101,12 +100,12 @@ func (b Base[T]) ListByUserId(ctx context.Context, userID string, limit int, off
 	return list, nil
 }
 
-func (b Base[T]) Update(ctx context.Context, ID string, updates any) error {
+func (b Base[T]) Update(ctx context.Context, id string, updates any) error {
 	names, keyToUpdate := common.KeysAndValues(updates)
 	if len(names) == 0 {
 		return errors.New("no fields to update")
 	}
-	query := fmt.Sprintf("UPDATE %s SET %s WHERE id = '%s'", b.Table, strings.Join(names, ", "), ID)
+	query := fmt.Sprintf("UPDATE %s SET %s WHERE id = '%s' AND deleted_at IS NULL", b.Table, strings.Join(names, ", "), id)
 	_, err := b.Store.NamedExecContext(ctx, query, keyToUpdate)
 	if err != nil {
 		return err
@@ -114,16 +113,16 @@ func (b Base[T]) Update(ctx context.Context, ID string, updates any) error {
 	return err
 }
 
-func (b Base[T]) Deactivate(ctx context.Context, ID string) error {
+func (b Base[T]) Deactivate(ctx context.Context, id string) error {
 	now := time.Now()
-	query := fmt.Sprintf("UPDATE %s SET deactivated_at = :time WHERE id = :id", b.Table)
-	_, err := b.Store.NamedExecContext(ctx, query, map[string]interface{}{"id": ID, "time": now})
+	query := fmt.Sprintf("UPDATE %s SET deactivated_at = :time WHERE id = :id AND deleted_at IS NULL", b.Table)
+	_, err := b.Store.NamedExecContext(ctx, query, map[string]interface{}{"id": id, "time": now})
 	return err
 }
 
-func (b Base[T]) Activate(ctx context.Context, ID string) error {
-	query := fmt.Sprintf("UPDATE %s SET deactivated_at = NULL WHERE id = :id", b.Table)
-	_, err := b.Store.NamedExecContext(ctx, query, map[string]interface{}{"id": ID})
+func (b Base[T]) Activate(ctx context.Context, id string) error {
+	query := fmt.Sprintf("UPDATE %s SET deactivated_at = NULL WHERE id = :id AND deleted_at IS NULL", b.Table)
+	_, err := b.Store.NamedExecContext(ctx, query, map[string]interface{}{"id": id})
 	return err
 }
 
@@ -137,4 +136,20 @@ func (b Base[T]) Get(ctx context.Context, model interface{}, query string, param
 
 func (b Base[T]) Named(query string, arg interface{}) (string, []interface{}, error) {
 	return sqlx.BindNamed(sqlx.DOLLAR, query, arg)
+}
+
+func (b Base[T]) SoftDelete(ctx context.Context, id string) error {
+	now := time.Now()
+	query := fmt.Sprintf("UPDATE %s SET deleted_at = :time WHERE id = :id AND deleted_at IS NULL", b.Table)
+	_, err := b.Store.NamedExecContext(ctx, query, map[string]interface{}{"id": id, "time": now})
+	return err
+}
+
+func (b Base[T]) IsDeleted(ctx context.Context, id string) (bool, error) {
+	var count int
+	err := b.Store.GetContext(ctx, &count, fmt.Sprintf("SELECT count(*) FROM %s WHERE id = $1 AND deleted_at IS NOT NULL", b.Table), id)
+	if err != nil {
+		return false, err
+	}
+	return count > 0, nil
 }
